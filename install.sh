2c4cecea5d3ee898416686b3af81b0c0e4c66b16
zaront <zaron.thompson@gmail.com>
2026-05-22 18:20:54 -0700

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,52 @@
 #! /bin/sh 
 
-echo "Installing dotfiles..."
+
+# DOWNLOAD DOTFILES
+
+# verify that the dotfiles are not already installed
+if [ -d "$HOME/.dotfiles" ]; then
+    echo "dotfiles already installed"
+    exit 1
+
+# clone the dotfiles if git is installed
+elif [ -x "$(command -v git)" ]; then
+    git clone https://github.com/zaron/dotfiles.git "$HOME/.dotfiles"
+
+# download the dotfiles with wget if its installed
+elif [ -x "$(command -v wget)" ]; then
+    mkdir -p "$HOME/.dotfiles" && wget -qO- https://github.com | tar -xz -C "$HOME/.dotfiles" --strip-components=1
+
+# download the dotfiles with curl if its installed
+elif [ -x "$(command -v curl)" ]; then
+    mkdir -p "$HOME/.dotfiles" && curl -L https://github.com | tar -xz -C "$HOME/.dotfiles" --strip-components=1
+ 
+# otherwise, print an error message
+else
+    echo "Dotfiles could not be installed. Missing git, wget, or curl"
+    exit 1
+fi
+
+
+# INSTALL DOTFILES
+
+# if on bash add sourcing shstart.sh to .bashrc if it doesn't already exist
+if [ -f "$HOME/.bashrc" ]; then
+    if ! grep -q "source $HOME/.dotfiles/shstart.sh" "$HOME/.bashrc"; then
+        echo "source $HOME/.dotfiles/shstart.sh" >> "$HOME/.bashrc"
+    fi
+
+    # if on alpine ash add sourcing shstart.sh to .profile if it doesn't already exist
+elif [ -f "$HOME/.profile" ]; then
+    if ! grep -q "export ENV='$HOME/.dotfiles/shstart.sh'" "$HOME/.profile"; then
+        echo "export ENV='$HOME/.dotfiles/shstart.sh'" >> "$HOME/.profile"
+    fi
+fi 
+
+
+# SOURCE DOTFILES
+source "$HOME/.dotfiles/shstart.sh"
+
+
+# COMPLETE
+echo "Dotfiles installed"
+echo "type 'dot' to get started"
--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,21 @@
 #! /bin/sh
+
+
+# UNINSTALL DOTFILES
+
+# if on bash remove sourcing shstart.sh from .bashrc if it exists
+if [ -f "$HOME/.bashrc" ]; then
+    sed -i '/\. "$HOME\/.dotfiles\/shstart.sh"/d' "$HOME/.bashrc"
+
+# if on alpine ash remove sourcing shstart.sh to .profile if it exists
+elif [ -f "$HOME/.profile" ]; then
+    sed -i '/\. "export ENV='$HOME\/.dotfiles\/shstart.sh'"/d' "$HOME/.profile"
+fi 
+
+
+# REMOVE DOTFILES
+rm -rf "$HOME/.dotfiles"
+
+
+# COMPLETE
+echo "Dotfiles have been uninstalled"
