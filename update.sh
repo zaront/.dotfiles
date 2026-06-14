c24ed25d48b6c3e71b99830f39cb8be0c2dfc359
zaront <zaron.thompson@gmail.com>
2026-06-13 17:18:20 -0700

    allowed updating outside the command - incase something breaks

---
+++ update.sh
@@ -0,0 +1,26 @@
+#! /bin/sh 
+
+#if installed with git, pull the latest changes
+if [ -d "$DOTFILES/.git" ]; then
+    cd "$DOTFILES" && git pull
+
+#otherwize us wget or curl to download the latest version and overwrite the existing files
+else
+
+    # download the dotfiles with wget if its installed
+    if [ -x "$(command -v wget)" ]; then
+        wget -qO- https://github.com/zaront/.dotfiles/archive/refs/heads/main.tar.gz | tar -xz -C "$DOTFILES" --strip-components=1
+
+# download the dotfiles with curl if its installed
+    elif [ -x "$(command -v curl)" ]; then
+        curl -L https://github.com/zaront/.dotfiles/archive/refs/heads/main.tar.gz | tar -xz -C "$DOTFILES" --strip-components=1
+
+    # otherwise, print an error message
+    else
+        echo "Dotfiles could not be updated. Missing git, wget, or curl"
+        exit 1
+    fi
+fi
+
+# complete
+echo "Dotfiles have been updated"
